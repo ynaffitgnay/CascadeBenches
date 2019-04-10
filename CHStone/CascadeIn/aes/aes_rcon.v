3e3ebe08ca4e63acab0580021827fef38fdbb086
Tiffany Yang <tiffanyyang@utexas.edu>
2019-04-10 01:08:11 -0500

    Replace function with module and wrote makeshift tb to test functionality

--- CHStone/CascadeIn/aes/aes_rcon.v
+++ CHStone/CascadeIn/aes/aes_rcon.v
@@ -54,27 +54,10 @@
 //
 //
 
-`include "timescale.v"
-
-module aes_rcon(clk, kld, out);
-input		clk;
-input		kld;
-output	[31:0]	out;
-reg	[31:0]	out;
-reg	[3:0]	rcnt;
-wire	[3:0]	rcnt_next;
-
-always @(posedge clk)
-	if(kld)		out <= #1 32'h01_00_00_00;
-	else		out <= #1 frcon(rcnt_next);
-
-assign rcnt_next = rcnt + 4'h1;
-always @(posedge clk)
-	if(kld)		rcnt <= #1 4'h0;
-	else		rcnt <= #1 rcnt_next;
-
-function [31:0]	frcon;
-input	[3:0]	i;
+module aes_frcon(i, frcon);
+input	wire [3:0]	i;
+output reg [31:0]	frcon;
+always @(i) begin
 case(i)	// synopsys parallel_case
    4'h0: frcon=32'h01_00_00_00;
    4'h1: frcon=32'h02_00_00_00;
@@ -87,7 +70,60 @@ case(i)	// synopsys parallel_case
    4'h8: frcon=32'h1b_00_00_00;
    4'h9: frcon=32'h36_00_00_00;
    default: frcon=32'h00_00_00_00;
-endcase
-endfunction
+endcase // case (i)
+end
+endmodule
+
+
+module aes_rcon(clk, kld, out);
+input	wire	clk;
+input	wire	kld;
+output reg	[31:0]	out;
+
+reg	[3:0]	rcnt;
+wire	[3:0]	rcnt_next;
+reg [31:0] intermediate_out;
+   
+aes_frcon frcon( .i(rcnt_next), .frcon(intermediate_out) );
+
+always @(posedge clk) begin
+   $display(rcnt_next);
+   
+   
+	if(kld)		out <= 32'h01_00_00_00;  // #1 means delay by 1 unit
+	else begin
+    //frcon( .i(rcnt_next), .frcon(intermediate_out) );
+    $display("%d", intermediate_out); 
+		out = intermediate_out;
+  end
+end   
+
+assign rcnt_next = rcnt + 4'h1;
+always @(posedge clk)
+	if(kld)		rcnt <= 4'h0;
+	else		rcnt <= rcnt_next;
+
+//function [31:0]	frcon;
+//input	[3:0]	i;
+//case(i)	// synopsys parallel_case
+//   4'h0: frcon=32'h01_00_00_00;
+//   4'h1: frcon=32'h02_00_00_00;
+//   4'h2: frcon=32'h04_00_00_00;
+//   4'h3: frcon=32'h08_00_00_00;
+//   4'h4: frcon=32'h10_00_00_00;
+//   4'h5: frcon=32'h20_00_00_00;
+//   4'h6: frcon=32'h40_00_00_00;
+//   4'h7: frcon=32'h80_00_00_00;
+//   4'h8: frcon=32'h1b_00_00_00;
+//   4'h9: frcon=32'h36_00_00_00;
+//   default: frcon=32'h00_00_00_00;
+//endcase
+//endfunction
 
 endmodule
+
+//todo: get rid of this
+reg[31:0] test;
+wire kld;
+assign kld = clock.val;
+aes_rcon rcon(clock.val, kld, test);
