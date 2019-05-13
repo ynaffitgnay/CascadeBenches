806bc64862cfa36579f92b151d14389245f7440d
Tiffany Yang <tiffanyyang@utexas.edu>
2019-05-13 17:48:08 -0500

    Completed get_motion_code

--- CascadeIn/mpeg/get_motion_code.v
+++ CascadeIn/mpeg/get_motion_code.v
@@ -1,19 +1,45 @@
-module get_motion_code(clk, rst, buf, inReady, outshift, done, mcode );
+module get_motion_code(clk, rst, buf, in_valid, outshift, done, mcode );
+  localparam ERROR = -1;
+
   input wire clk;
   input wire rst;
   input wire [19:0] buf;
-  input wire inReady;
+  input wire in_valid;
   
-  output reg [4:0] outshift;
+  output wire [4:0] outshift;
   output reg done;
-  output mcode;
-
-  localparam ERROR = 17;
-
+  output wire signed[4:0] mcode;
 
   wire [4:0] MVtab0[7:0][1:0];
   wire [4:0] MVtab1[7:0][1:0];
   wire [4:0] MVtab2[11:0][1:0];
+
+  wire [8:0] code1;
+  wire [31:0] code2;
+  wire [4:0] check_output_bit;
+  wire signed[4:0] mcode1;
+
+
+  always @(posedge clk) begin
+    if (rst)
+      done <= 1'b0;
+    else if (in_valid) begin      
+      done <= 1'b1;
+      $display("code2: %d, check_output_bit: %d", code2, check_output_bit);
+
+    end
+
+
+  end
+
+  assign code1 = buf[18:10];
+  
+  assign code2 = (code1 >= 64) ? (code1 >> 6) : ((code1 >= 24) ? code1 >> 3 : code1);
+  assign outshift = 1 + (buf[19] ? 0 : ((code1 >= 64) ? ((MVtab0[code2][1]) + 1) : ((code1 >= 24) ? (MVtab1[code2][1] + 1) : ((code1 < 12) ? 0 : (MVtab2[code2][1] + 1)))));
+  assign mcode1 = (code1 >= 64) ? MVtab0[code2][0] : ((code1 >= 24) ? MVtab1[code2][0] : ((code1 < 12) ? 0 : (MVtab2[code2][1])));
+  assign check_output_bit = 19 - (outshift - 1);
+  assign mcode = buf[19] ? 0 : ((code1 < 12) ? 0 : (buf[check_output_bit] ? (mcode1 * -1) : mcode1));
+ 
   
 
   assign MVtab0[0][0] = ERROR;
@@ -81,13 +107,35 @@ endmodule // get_motion_code
 
 
 reg[19:0] buf;
-reg inReady;
+reg in_valid;
 reg rst;
 reg[5:0] outshift;
 reg done;
-reg mcode;
+reg signed[4:0] mcode;
+reg[31:0] ctr = 0;
 
 
+initial begin
+  buf = 20'b1000111100001111;
+//20'h7ffff;
+//20'b1000111100001111;
+  in_valid = 1;
+  rst = 0;
+  
 
-get_motion_code gmc(clock.val, rst, buf, inReady, outshift, done, mcode);
+end
+
+always @(posedge clock.val) begin
+  ctr <= ctr + 1;
+
+  $display("outshift: %d, mcode: %d", outshift, mcode);
+
+  if (done)
+    $display("final: outshift: %d, mcode: %d", outshift, mcode);
+
+  if (ctr == 5) $finish;
+
+end
+
+get_motion_code gmc(clock.val, rst, buf, in_valid, outshift, done, mcode);
 
