8c804caf3295fa8c895619685f2a7f13ed0de022
Tiffany Yang <tiffanyyang@utexas.edu>
2020-04-07 14:33:22 -0500

    More cleanup

--- bugreports/implied_latches.v
+++ bugreports/implied_latches.v
@@ -1,23 +1,3 @@
-`define C_LOG_2(n) (\
-(n) <= (1<<0) ? 0 : (n) <= (1<<1) ? 1 :\
-(n) <= (1<<2) ? 2 : (n) <= (1<<3) ? 3 :\
-(n) <= (1<<4) ? 4 : (n) <= (1<<5) ? 5 :\
-(n) <= (1<<6) ? 6 : (n) <= (1<<7) ? 7 :\
-(n) <= (1<<8) ? 8 : (n) <= (1<<9) ? 9 :\
-(n) <= (1<<10) ? 10 : (n) <= (1<<11) ? 11 :\
-(n) <= (1<<12) ? 12 : (n) <= (1<<13) ? 13 :\
-(n) <= (1<<14) ? 14 : (n) <= (1<<15) ? 15 :\
-(n) <= (1<<16) ? 16 : (n) <= (1<<17) ? 17 :\
-(n) <= (1<<18) ? 18 : (n) <= (1<<19) ? 19 :\
-(n) <= (1<<20) ? 20 : (n) <= (1<<21) ? 21 :\
-(n) <= (1<<22) ? 22 : (n) <= (1<<23) ? 23 :\
-(n) <= (1<<24) ? 24 : (n) <= (1<<25) ? 25 :\
-(n) <= (1<<26) ? 26 : (n) <= (1<<27) ? 27 :\
-(n) <= (1<<28) ? 28 : (n) <= (1<<29) ? 29 :\
-(n) <= (1<<30) ? 30 : (n) <= (1<<31) ? 31 : 32)
-//-----------------------------------------------------------
-
-
 `define AMI_ADDR_WIDTH 64
 `define AMI_DATA_WIDTH (512 + 64)
 `define AMI_REQ_SIZE_WIDTH 6
@@ -28,10 +8,6 @@
 `define AMIRequest_addr        (`AMI_ADDR_WIDTH + 2 - 1):2
 `define AMIRequest_data        (`AMI_DATA_WIDTH + `AMI_ADDR_WIDTH + 2 - 1):(`AMI_ADDR_WIDTH + 2)
 `define AMIRequest_size        (`AMI_REQ_SIZE_WIDTH + `AMI_DATA_WIDTH + `AMI_ADDR_WIDTH + 2 - 1):(`AMI_DATA_WIDTH + `AMI_ADDR_WIDTH + 2)
-`define AMI_RESPONSE_BUS_WIDTH  (1 + `AMI_DATA_WIDTH + `AMI_REQ_SIZE_WIDTH)
-`define AMIResponse_valid       0:0
-`define AMIResponse_data        (`AMI_DATA_WIDTH + 1 - 1):1
-`define AMIResponse_size        (`AMI_REQ_SIZE_WIDTH + `AMI_DATA_WIDTH + 1 - 1):(`AMI_DATA_WIDTH + 1)
 
 module we_decoder(
     input we_all,
@@ -63,26 +39,23 @@ module BlockBuffer
 
     // Params
     localparam NUM_SECTORS  = 8;
-    localparam SECTOR_WIDTH = 64;
     
     // Sectors
-    wire[(NUM_SECTORS*SECTOR_WIDTH)-1:0] wr_output;
     wire[NUM_SECTORS-1:0] sector_we;
     
     // Queue for incoming AMIRequests
     wire[`AMI_REQUEST_BUS_WIDTH - 1:0]       reqInQ_out;    
 
     // Read data out of the block
-    //wire [SECTOR_WIDTH-1:0] rd_output;
-    reg [`C_LOG_2(NUM_SECTORS)-1:0] rd_mux_sel; // controlled by the FSM
+    reg [2:0] rd_mux_sel; // controlled by the FSM
 
     // FSM signals
     reg wr_all_sectors;
     reg wr_specific_sector;
 
     /*-------------------------- VAR WE CARE ABOUT------------------------- */
-    reg[`C_LOG_2(NUM_SECTORS)-1:0] wr_sector_index;
-    //wire [`C_LOG_2(NUM_SECTORS)-1:0] wr_sector_index;
+    reg[2:0] wr_sector_index;
+    //wire [2:0] wr_sector_index;
     
     we_decoder
     writes_decoder
@@ -93,10 +66,6 @@ module BlockBuffer
         .we_out      (sector_we)
     );
 
-    
-    /////////////////////
-    // FSM
-    /////////////////////
     
     wire[`AMI_ADDR_WIDTH - 1:0] reqInQ_out_addr;
     assign reqInQ_out_addr = reqInQ_out[`AMIRequest_addr];
