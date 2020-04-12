82a37d332c025be3e7c88bfecb1c4dec2b867397
Tiffany Yang <tiffanyyang@utexas.edu>
2020-04-12 16:26:24 -0500

    Make fake fifo even more bare bones

--- bugreports/cascade_hang.v
+++ bugreports/cascade_hang.v
@@ -17,35 +17,12 @@ module SoftFIFO  #(parameter WIDTH = 512, LOG_DEPTH = 9)
 
 
 reg [WIDTH-1:0] buffer[(1 << LOG_DEPTH)-1:0];
-
 reg [LOG_DEPTH:0] counter;
-reg [LOG_DEPTH:0]  new_counter;
-reg [LOG_DEPTH-1:0] rd_ptr, wr_ptr; 
-reg [LOG_DEPTH-1:0]  new_rd_ptr, new_wr_ptr;
+reg [LOG_DEPTH-1:0] rd_ptr;
 
 assign empty = (counter == 0);
 assign full  = (counter == (1 << LOG_DEPTH));
 assign q     = buffer[rd_ptr];
-
-always @(posedge clock) begin
-    if (!reset_n) begin
-        counter <= 0;
-        rd_ptr  <= 0;
-        wr_ptr  <= 0;
-    end else begin
-        counter <= new_counter;
-        rd_ptr  <= new_rd_ptr;
-        wr_ptr  <= new_wr_ptr;
-    end
-end
-
-always @(posedge clock) begin
-    if (!full && wrreq) begin
-        buffer[wr_ptr] <= data;
-    end else begin
-        buffer[wr_ptr] <= buffer[wr_ptr];
-    end
-end
 
 endmodule // SoftFIFO
 
