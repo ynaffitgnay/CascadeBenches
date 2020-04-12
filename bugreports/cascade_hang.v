ad0c60ef4c3bfb33fcfc214009331dbdb52751ec
Tiffany Yang <tiffanyyang@utexas.edu>
2020-04-12 16:23:30 -0500

    Remove combinational logic from fake fifo defn

--- bugreports/cascade_hang.v
+++ bugreports/cascade_hang.v
@@ -1,25 +1,6 @@
 `ifndef __DNN2AMI_WRPath_sv__
 `define __DNN2AMI_WRPath_sv__
 
-`define C_LOG_2(n) (\
-(n) <= (1<<0) ? 0 : (n) <= (1<<1) ? 1 :\
-(n) <= (1<<2) ? 2 : (n) <= (1<<3) ? 3 :\
-(n) <= (1<<4) ? 4 : (n) <= (1<<5) ? 5 :\
-(n) <= (1<<6) ? 6 : (n) <= (1<<7) ? 7 :\
-(n) <= (1<<8) ? 8 : (n) <= (1<<9) ? 9 :\
-(n) <= (1<<10) ? 10 : (n) <= (1<<11) ? 11 :\
-(n) <= (1<<12) ? 12 : (n) <= (1<<13) ? 13 :\
-(n) <= (1<<14) ? 14 : (n) <= (1<<15) ? 15 :\
-(n) <= (1<<16) ? 16 : (n) <= (1<<17) ? 17 :\
-(n) <= (1<<18) ? 18 : (n) <= (1<<19) ? 19 :\
-(n) <= (1<<20) ? 20 : (n) <= (1<<21) ? 21 :\
-(n) <= (1<<22) ? 22 : (n) <= (1<<23) ? 23 :\
-(n) <= (1<<24) ? 24 : (n) <= (1<<25) ? 25 :\
-(n) <= (1<<26) ? 26 : (n) <= (1<<27) ? 27 :\
-(n) <= (1<<28) ? 28 : (n) <= (1<<29) ? 29 :\
-(n) <= (1<<30) ? 30 : (n) <= (1<<31) ? 31 : 32)
-
-
 module SoftFIFO  #(parameter WIDTH = 512, LOG_DEPTH = 9)
 (
     // General signals
@@ -66,71 +47,13 @@ always @(posedge clock) begin
     end
 end
 
-always @(*) begin
-    if ((!full && wrreq) && (!empty && rdreq)) begin
-        new_counter = counter;
-        new_rd_ptr  = rd_ptr + 1;
-        new_wr_ptr  = wr_ptr + 1;
-    end else if (!full && wrreq) begin
-        new_counter = counter + 1;
-        new_rd_ptr  = rd_ptr;
-        new_wr_ptr  = wr_ptr + 1;
-    end else if (!empty && rdreq) begin
-        new_counter = counter - 1;
-        new_rd_ptr  = rd_ptr + 1;
-        new_wr_ptr  = wr_ptr;
-    end else begin
-        new_counter = counter;
-        new_rd_ptr = rd_ptr;
-        new_wr_ptr = wr_ptr;
-    end
-end
-
 endmodule // SoftFIFO
 
 
 
 module DNN2AMI_WRPath
 #(
-  parameter integer NUM_PU               = 2,
-
-  parameter integer AXI_ID               = 0,
-
-  parameter integer TID_WIDTH            = 6,
-  parameter integer AXI_ADDR_WIDTH       = 32,
-  parameter integer AXI_DATA_WIDTH       = 64,
-  parameter integer AWUSER_W             = 1,
-  parameter integer ARUSER_W             = 1,
-  parameter integer WUSER_W              = 1,
-  parameter integer RUSER_W              = 1,
-  parameter integer BUSER_W              = 1,
-
-  /* Disabling these parameters will remove any throttling.
-   The resulting ERROR flag will not be useful */
-  parameter integer C_M_AXI_SUPPORTS_WRITE             = 1,
-  parameter integer C_M_AXI_SUPPORTS_READ              = 1,
-
-  /* Max count of written but not yet read bursts.
-   If the interconnect/slave is able to accept enough
-   addresses and the read channels are stalled, the
-   master will issue this many commands ahead of
-   write responses */
-
-  // Base address of targeted slave
-  //Changing read and write addresses
-  parameter         C_M_AXI_READ_TARGET                = 32'hFFFF0000,
-  parameter         C_M_AXI_WRITE_TARGET               = 32'hFFFF8000,
-
-  // CUSTOM PARAMS
-  parameter         TX_SIZE_WIDTH                      = 10,
-
-  // Number of address bits to test before wrapping
-  parameter integer C_OFFSET_WIDTH                     = TX_SIZE_WIDTH,
- 
-  parameter integer WSTRB_W  = AXI_DATA_WIDTH/8,
-  parameter integer NUM_PU_W = `C_LOG_2(NUM_PU)+1,
-  parameter integer OUTBUF_DATA_W = NUM_PU * AXI_DATA_WIDTH
- 
+  parameter integer NUM_PU               = 2 
 )
 (
     // General signals
@@ -184,10 +107,9 @@ module DNN2AMI_WRPath
             outbuf_pop[i] = 1'b0;
         end
         
-        // See if there is a new operation available
         if (!macroWrQ_empty) begin
         end
-    end // always @ (*)
+    end
     
 endmodule
 `endif //  `ifndef __DNN2AMI_WRPath_sv__
